41cf62db6626bab7907709d467a2607786bbd564
Vadim Bezrukov <bezrukoff.va@gmail.com>
2024-05-04 14:47:26 +0300

    add some values

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello World!");
+﻿// Console.WriteLine("Hello World!");
+
+int a = 100;
+Console.WriteLine(a);
+
+double b = 5.55;
+char c = '!';
+bool d = true;
+
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
